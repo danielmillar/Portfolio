91af411892138bfdbb992aea1118347cbba1c0cc
Daniel Millar <millargdaniel@outlook.com>
2024-11-30 14:23:33 +0000

    feat(): Add X svg logo

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,4 +1,4 @@
-import { Mail, GithubIcon, X } from 'lucide-react';
+import { Mail, GithubIcon } from 'lucide-react';
 
 interface ContactLink {
   platform: string;
@@ -23,7 +23,16 @@ const contactLinks: ContactLink[] = [
   {
     platform: "X",
     href: "https://x.com/danielgmillar",
-    icon: <X className="w-5 h-5" />,
+    icon: <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width="20" 
+      height="20" 
+      viewBox="0 0 1200 1227" 
+      fill="currentColor"
+      className="w-5 h-5 text-blue-600 dark:text-blue-400"
+    >
+      <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"/>
+    </svg>,
     username: "@danielgmillar"
   }
 ];
