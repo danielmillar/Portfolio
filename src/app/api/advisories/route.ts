01370eb6b83628bbb5949621376f4f484f7da502
Daniel Millar <millargdaniel@outlook.com>
2025-02-23 19:35:39 +0000

    Update route.ts

--- src/app/api/advisories/route.ts
+++ src/app/api/advisories/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import clientPromise from '@/data/mongodb'
 
+export const runtime = 'edge'
+
 export async function GET() {
   try {
     const client = await clientPromise
