bfd19f3858c811fa2f0211d3dd46599c8638e131
Daniel Millar <millargdaniel@outlook.com>
2025-02-23 19:52:47 +0000

    Update route URL

--- src/app/api/advisories/route.ts
+++ src/app/api/advisories/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const response = await fetch('https://faa-serverless-function-df1tq0cf1-danielmillars-projects.vercel.app/api/advisories');
+  const response = await fetch('https://faa-serverless-function.vercel.app/api/advisories');
   const data = await response.json();
   return NextResponse.json(data);
 }
